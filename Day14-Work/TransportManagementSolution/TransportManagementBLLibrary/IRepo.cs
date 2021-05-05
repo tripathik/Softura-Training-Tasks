fab9e14cdfca175c0f9ffa04f1cf70ccb22ec745
KRISHNA TRIPATHI <56901924+tripathik@users.noreply.github.com>
2021-05-05 11:29:34 +0530

    Add files via upload

---
+++ Day14-Work/TransportManagementSolution/TransportManagementBLLibrary/IRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportManagementBLLibrary
+{
+   public interface IRepo<T>
+    {
+        void Add(T t);
+        void Update(int id, T t);
+        IEnumerable<T> GetAll(); //
+        //ICollection<T> GetAll();
+        T Get(int id);
+
+        void Delete(int id);
+    }
+}

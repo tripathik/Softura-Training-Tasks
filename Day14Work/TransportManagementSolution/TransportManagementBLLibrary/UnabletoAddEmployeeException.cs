563eb32f8f32de55c400ceba0c482a8c31572638
KRISHNA TRIPATHI <56901924+tripathik@users.noreply.github.com>
2021-05-05 11:34:39 +0530

    Add files via upload

---
+++ Day14Work/TransportManagementSolution/TransportManagementBLLibrary/UnabletoAddEmployeeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportManagementBLLibrary
+{
+    public class UnabletoAddEmployeeException : ApplicationException
+    {
+        string _message;
+        public UnabletoAddEmployeeException()
+        {
+            _message = "Unable to Add employee cod of Id duplication. Please Try again!!!";
+        }
+        public override string Message => _message;
+    }
+}

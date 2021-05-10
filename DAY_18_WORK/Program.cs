6a8a90017ad12a32f3137e54762c8d2ace9f5e1e
KRISHNA TRIPATHI <56901924+tripathik@users.noreply.github.com>
2021-05-10 19:40:35 +0530

    Add files via upload

---
+++ DAY_18_WORK/Program.cs
@@ -0,0 +1,47 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//namespace LINQEgProject
+//{
+//    class Program
+//    {
+//      static void Main(string[] args)
+//      {
+
+
+//    //        List<int> mynum = new List<int>();
+//    //        mynum.Add(10);
+//    //        mynum.Add(25);
+//    //        mynum.Add(37);
+//    //        mynum.Add(25);
+//    //        mynum.Add(60);
+//    //        mynum.Add(19);
+//    //        mynum.Add(100);
+//    //        mynum.Add(65);
+//    //        mynum.Add(40);
+//    //        var result1 = mynum.Where(i => i<50).Select(s => s).OrderBy(i=>i);
+//    //        foreach (var item in result1)
+//    //        {
+//    //            Console.WriteLine(item);
+//    //        }
+
+//    //        List<string> fruits = new List<string>();
+//    //        fruits.Add("Apple");
+//    //        fruits.Add("Ape");
+//    //        fruits.Add("Apricot");
+//    //        fruits.Add("Kivi");
+//    //        fruits.Add("Avacado");
+//    //        fruits.Add("Banana");
+//    //        var result = (from s in fruits
+//    //                      where  s.StartsWith('A')
+//    //                      select s).ToList();
+
+//    //        var res = fruits/*.Where(s => s.StartsWith('A') )*/ .Select(s => s).OrderBy(s=>s);
+//    //        foreach (var item in res)
+//    //        {
+//    //            Console.WriteLine(item);
+//    //        }
+            
+//   }
+//    }
+//}

976ac14b75cfe198f7e887919f5ae0e334da5ed2
KRISHNA TRIPATHI <56901924+tripathik@users.noreply.github.com>
2021-05-11 16:06:20 +0530

    Add files via upload

---
+++ DAY_19_WORK/EFandLINQProject/Models/TweetContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFandLINQProject.Models
+{
+    class TweetContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-EBPUJ98\SQLEXPRESS;Integrated Security=true;Database=dbTweet");
+
+            }
+        }
+        public DbSet<Post>Posts { get; set; }
+        public DbSet<Comment>Comments { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasData(new Post
+            {
+                Id=1,
+                PostText="Test",
+                Category="Info"
+            });
+        }
+    }
+}

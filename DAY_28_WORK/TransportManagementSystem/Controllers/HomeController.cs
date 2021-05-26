17d87b111d8ee54c4d09aa65e2019ad81fd019c0
KRISHNA TRIPATHI <56901924+tripathik@users.noreply.github.com>
2021-05-26 09:19:49 +0530

    Add files via upload

---
+++ DAY_28_WORK/TransportManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TransportManagementSystem.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace TransportManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public HomeController(UserManager<IdentityUser>userManager,
+                               SignInManager<IdentityUser>signInManager, ILogger<HomeController> logger)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            _logger = logger;
+        }
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        public async Task<IActionResult> LogOut()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async  Task<IActionResult> Register( RegisterModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.UserName };
+                var result = await userManager.CreateAsync(user, model.Password);
+                if(result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index","home");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                {
+
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.UserName, model.Password,
+                                    model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+                    ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ DAY_28_WORK/TransportManagementSystem/Controllers/RoutesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TransportManagementSystem.Models;
+
+namespace TransportManagementSystem.Controllers
+{
+    public class RoutesController : Controller
+    {
+        private readonly TMSContext _context;
+
+        public RoutesController(TMSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Routes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Routes.ToListAsync());
+        }
+
+        // GET: Routes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var route = await _context.Routes
+                .FirstOrDefaultAsync(m => m.RouteNumber == id);
+            if (route == null)
+            {
+                return NotFound();
+            }
+
+            return View(route);
+        }
+
+        // GET: Routes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Routes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RouteNumber,Stops,VehicleNumber")] Route route)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(route);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(route);
+        }
+
+        // GET: Routes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var route = await _context.Routes.FindAsync(id);
+            if (route == null)
+            {
+                return NotFound();
+            }
+            return View(route);
+        }
+
+        // POST: Routes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RouteNumber,Stops,VehicleNumber")] Route route)
+        {
+            if (id != route.RouteNumber)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(route);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RouteExists(route.RouteNumber))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(route);
+        }
+
+        // GET: Routes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var route = await _context.Routes
+                .FirstOrDefaultAsync(m => m.RouteNumber == id);
+            if (route == null)
+            {
+                return NotFound();
+            }
+
+            return View(route);
+        }
+
+        // POST: Routes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var route = await _context.Routes.FindAsync(id);
+            _context.Routes.Remove(route);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RouteExists(int id)
+        {
+            return _context.Routes.Any(e => e.RouteNumber == id);
+        }
+    }
+}

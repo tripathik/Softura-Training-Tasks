47b1cffb95b3dc8165c499075719fb7c68abcbfd
KRISHNA TRIPATHI <56901924+tripathik@users.noreply.github.com>
2021-06-21 14:51:26 +0530

    Add files via upload

---
+++ JUNE_21_WORK/Controllers/BookController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Configuration;
+using Dapper;
+using MVCusgDPR.Models;
+using System.Data;
+
+namespace MVCusgDPR.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            List<BookModel> Bklist = new List<BookModel>();
+            using(IDbConnection dbcon=new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                Bklist = dbcon.Query<BookModel>("select * from tbl_Books").ToList();
+            }
+            return View(Bklist);
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            BookModel bk = new BookModel();
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                bk = dbcon.Query<BookModel>("select * from tbl_Books where bookid=" +id,new { id}).SingleOrDefault();
+            }
+            return View(bk);
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        public ActionResult Create(BookModel bmodel)
+        {
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                string SqlQry = "insert into tbl_Books(Title,AuthorId,Price)values('" + bmodel.Title + "'," + bmodel.AuthorId + "," + bmodel.Price + " )";
+                int rowins = dbcon.Execute(SqlQry);
+            }
+
+
+            return RedirectToAction("Index");
+            
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            BookModel bk = new BookModel();
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                bk = dbcon.Query<BookModel>("select * from tbl_Books where bookid=" + id, new { id }).SingleOrDefault();
+            }
+            return View(bk);
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(BookModel bk)
+        {
+
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                string SqlQry = "update tbl_Books set Title='" + bk.Title + "',AuthorId=" + bk.AuthorId + ",Price=" + bk.Price + " where BookId="+bk.BookId;
+                int rowins = dbcon.Execute(SqlQry);
+            }
+            return RedirectToAction("Index");
+            
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            BookModel bk = new BookModel();
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                bk = dbcon.Query<BookModel>("select * from tbl_Books where bookid=" + id, new { id }).SingleOrDefault();
+            }
+            return View(bk);
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            BookModel bk = new BookModel();
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                bk = dbcon.Query<BookModel>("delete from tbl_Books where bookid=" + id, new { id }).SingleOrDefault();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ JUNE_21_WORK/Controllers/MoviesController.cs
@@ -0,0 +1,107 @@
+﻿using MVCusgDPR.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dapper;
+
+namespace MVCusgDPR.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            List<MoviesModel> MvList = new List<MoviesModel>();
+            using(IDbConnection dbcon=new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                MvList= dbcon.Query<MoviesModel>("select * from tbl_Movies").ToList();
+            }
+            return View(MvList);
+        }
+
+     
+        public ActionResult Details(int num)
+        {
+            MoviesModel bk = new MoviesModel();
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                bk = dbcon.Query<MoviesModel>("select * from tbl_Movies where Number=" + num, new { num }).SingleOrDefault();
+            }
+            return View(bk);
+        }
+
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult Create(MoviesModel bmodel)
+        {
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                string SqlQry = "insert into tbl_Movies(Movie_Name)values('" + bmodel.Movie_Name + "')";
+                int rowins = dbcon.Execute(SqlQry);
+            }
+
+
+            return RedirectToAction("Index");
+
+        }
+
+      
+        public ActionResult Edit(int num)
+        {
+            MoviesModel bk = new MoviesModel();
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                bk = dbcon.Query<MoviesModel>("select * from tbl_Movies where Number=" + num, new { num }).SingleOrDefault();
+            }
+            return View(bk);
+        }
+
+     
+        [HttpPost]
+        public ActionResult Edit(MoviesModel bk)
+        {
+
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                string SqlQry = "update tbl_Books set Movie_Name='" + bk.Movie_Name + "' where Number=" + bk.Number;
+                int rowins = dbcon.Execute(SqlQry);
+            }
+            return RedirectToAction("Index");
+
+        }
+
+     
+        public ActionResult Delete(int num)
+        {
+            MoviesModel bk = new MoviesModel();
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                bk = dbcon.Query<MoviesModel>("select * from tbl_Movies where Number=" + num, new { num }).SingleOrDefault();
+            }
+            return View(bk);
+            
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int num, FormCollection collection)
+        {
+            MoviesModel bk = new MoviesModel();
+            using (IDbConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["BKConStr"].ConnectionString))
+            {
+                bk = dbcon.Query<MoviesModel>("delete from tbl_Movies where Number=" + num, new { num }).SingleOrDefault();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}

711b4f537b6d05d616d717d562c66f9454553f8d
KRISHNA TRIPATHI <56901924+tripathik@users.noreply.github.com>
2021-06-22 00:59:47 +0530

    Add files via upload

---
+++ June_18_Task/Content/Controllers/MultipleDDController.cs
@@ -0,0 +1,47 @@
+﻿using MultipleDropDownList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultipleDropDownList.Controllers
+{
+    public class MultipleDDController : Controller
+    {
+        public ActionResult Index()
+        {
+            MDDLEntity entities = new MDDLEntity();
+            MultipleDDModel model = new MultipleDDModel();
+            foreach (var product in entities.ProductCategories)
+            {
+                model.ProductCategories.Add(new SelectListItem { Text = product.Name, Value = product.ProductCategoryID.ToString() });
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(int? ProductCategoryID, int? ProductSubcategoryID)
+        {
+            MultipleDDModel model = new MultipleDDModel();
+            MDDLEntity entities = new MDDLEntity();
+            foreach (var product in entities.ProductCategories)
+            {
+                model.ProductCategories.Add(new SelectListItem { Text = product.Name, Value = product.ProductCategoryID.ToString() });
+            }
+
+            if (ProductCategoryID.HasValue)
+            {
+                var subcats = (from ProductSubcategory in entities.ProductSubcategories
+                              where ProductSubcategory.ProductCategoryID == ProductCategoryID.Value
+                              select ProductSubcategory).ToList();
+                foreach (var ProductSubcategory in subcats)
+                {
+                    model.ProductSubcategories.Add(new SelectListItem { Text = ProductSubcategory.Name, Value = ProductSubcategory.ProductSubcategoryID.ToString() });
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ June_18_Task/Content/Models/MultipleDDModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultipleDropDownList.Models
+{
+    public class MultipleDDModel
+    {
+        public MultipleDDModel()
+        {
+            
+            this.ProductCategories = new List<SelectListItem>();
+            this.ProductSubcategories = new List<SelectListItem>();
+        }
+
+      
+        public List<SelectListItem> ProductCategories { get; set; }
+        public List<SelectListItem> ProductSubcategories { get; set; }
+
+      
+        public int ProductCategoryID { get; set; }
+        public int ProductSubcategoryID { get; set; }
+    }
+}

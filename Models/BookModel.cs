8b8e4bb5e622eb3ea2df87479317fb17ea3ed46d
KRISHNA TRIPATHI <56901924+tripathik@users.noreply.github.com>
2021-06-21 14:53:00 +0530

    Add files via upload

---
+++ Models/BookModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCusgDPR.Models
+{
+    public class BookModel
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Models/MoviesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCusgDPR.Models
+{
+    public class MoviesModel
+    {
+        public int Number { get; set; }
+        public string Movie_Name { get; set; }
+    }
+}
